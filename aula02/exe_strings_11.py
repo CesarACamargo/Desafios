d349da0106697b6c3aa0f723d176d331ef2e8a1b
Caesar <caesar.camargo@gmail.com>
2026-05-22 12:42:35 -0300

    :arrow_up: Subindo novos exercicios.

---
+++ aula02/exe_strings_11.py
@@ -0,0 +1,5 @@
+# 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+palavra = input("Digite uma palavra: ")
+maiusculas = palavra.upper()
+
+print(maiusculas)
---
+++ aula02/exe_strings_15.py
@@ -0,0 +1,7 @@
+# 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+s1 = input("Digite uma palavra: ")
+s2 = input("Digite outra palavra: ")
+
+print(s1 + s2)
+
