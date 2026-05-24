4e485dcbd49e863578a57f0f32fef9e3f940407d
Caesar <caesar.camargo@gmail.com>
2026-05-24 13:49:18 -0300

    :ok_hand: Subindo exe_05_ctrl_fluxos.py da aula03.

--- aula03/exe_05_ctrl_fluxos.py
+++ aula03/exe_05_ctrl_fluxos.py
@@ -4,3 +4,8 @@
 # a R$ 10.000 ou se ocorrer fora do horário comercial (antes das 9h ou depois das 18h). 
 # Dada uma transação como `transacao = {'valor': 12000, 'hora': 20}`, verifique se ela é suspeita.
 
+transacao = {'valor': 12000, 'hora': 20}
+
+if transacao['valor'] >= 10000 or transacao['hora'] < 9 or transacao['hora'] > 18 :
+    print("Transação SUSPEITA !") 
+
