85a490efc57ce16943d127426ef5d1e53cb5e340
Caesar <caesar.camargo@gmail.com>
2026-06-09 23:17:48 -0300

    :ok_hand: Subindo atualizações novas.

--- aula04/exe_01_aula04.py
+++ aula04/exe_01_aula04.py
@@ -1,6 +1,12 @@
 # 1) Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
 
+<<<<<<< HEAD
 numeros: list = [1,2,3,4,5,6,7,8,9,10]
 
 for n in numeros :
+=======
+numeros: list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for n in numeros:
+>>>>>>> b2aa56f (:ok_hand: Subindo atualizações novas.)
     print(f"O quadrado de {n} é {n ** 2}")
