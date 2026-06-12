8d5766b5ba4f2150dc6cbd78c370bb48063d3c66
Caesar <caesar.camargo@gmail.com>
2026-06-12 18:40:21 -0300

    :ok_hand: Subindo codigos atualizados.

--- aula07/projeto_desafio/etl.py
+++ aula07/projeto_desafio/etl.py
@@ -3,7 +3,9 @@ import csv
 path_arquivo = "vendas.csv"
 
 def ler_csv(nome_do_arquivo_csv: str) -> list[dict]:
-
+    """
+    Função que lê um arquivo csv e retorna uma lista de dicionarios
+    """
     lista = []
     with open(nome_do_arquivo_csv, mode="r", encoding='utf-8') as arquivo_csv:
         leitor_csv = csv.DictReader(arquivo_csv)
@@ -11,7 +13,23 @@ def ler_csv(nome_do_arquivo_csv: str) -> list[dict]:
             lista.append(linha)
     return lista
 
-vendas_itens: list[dict]
 
-vendas_itens = ler_csv(path_arquivo)
-print(vendas_itens)
+def filtrar_produtos_nao_entregues(lista: list[dict]) -> list[dict]:
+    """
+    Função que filtra produtos onde entrega = True
+    """
+    lista_com_produtos_filtrados = []
+    for produto in lista:
+        if produto.get("Entregue") == "True":
+            lista_com_produtos_filtrados.append(produto)
+    return lista_com_produtos_filtrados
+
+
+def somar_valores_dos_produtos(lista_com_produtos_filtrados: list[dict]) -> int:
+    """
+    Soma todos os produtos que estão na lista
+    """
+    valor_total = 0
+    for produto in lista_com_produtos_filtrados:
+        valor_total += int(produto.get("Price"))
+    return valor_total
